b727394a5f684334fbe8c9953d2b187c481ab6ba
EnzoCam7 <camerac09@gmail.com>
2023-11-12 16:20:22 -0300

    Feat: added select for valoracion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
-import { useState } from 'react';
-import {preguntas} from '../data.json'
+import { ChangeEvent, useState } from 'react';
+import data from '../data.json'
 
 interface Question {
   id:    number;
@@ -12,15 +12,30 @@ interface Answer extends Question{
 }
 
 
-const questions: Question[] = preguntas;
+const questions: Question[] =  data.preguntas;
 
 export default function Home() {
   
   const [answers, setAnswers] = useState<Answer[]>([])
+  const currentQuestion = questions[answers.length];
+
+  function handleRate(event: ChangeEvent<HTMLSelectElement>){
+    setAnswers(answers => answers.concat({
+      ...currentQuestion,
+      valoracion: Number(event.target.value) as Answer['valoracion']
+    }))
+  } 
 
   return (
     <main >
-      <h1>{questions[0].texto}</h1>
+      <h1>{currentQuestion.texto}</h1>
+      <select onChange={handleRate}>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
     </main>
   )
 }
