423077c330d1def98e67736cd428e4eb8ae8a0a6
EnzoCam7 <camerac09@gmail.com>
2023-11-12 16:44:20 -0300

    Feat: add rating component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,66 @@
-'use client'
-import { ChangeEvent, useState } from 'react';
-import data from '../data.json'
+"use client";
+import { ChangeEvent, useState } from "react";
+import data from "../data.json";
 
 interface Question {
-  id:    number;
+  id: number;
   texto: string;
 }
-
-interface Answer extends Question{
-  valoracion: 1 | 2 | 3 | 4 | 5
+interface Answer extends Question {
+  valoracion: 1 | 2 | 3 | 4 | 5;
 }
 
+const questions: Question[] = data.preguntas;
 
-const questions: Question[] =  data.preguntas;
-
-export default function Home() {
+// Los componentes de tipo INPUT suelen recibir siempre un onChange y un value
+function Rating({value, onChange} : {value: Answer["valoracion"]; onChange:(value: number) => void}, ) {
   
-  const [answers, setAnswers] = useState<Answer[]>([])
-  const currentQuestion = questions[answers.length];
-
-  function handleRate(event: ChangeEvent<HTMLSelectElement>){
-    setAnswers(answers => answers.concat({
-      ...currentQuestion,
-      valoracion: Number(event.target.value) as Answer['valoracion']
-    }))
-  } 
+  function handleRate(event: ChangeEvent<HTMLSelectElement>) {
+    onChange(Number(event.target.value) as Answer["valoracion"])
+  }
 
   return (
-    <main >
+    <select value={value} onChange={handleRate}>
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+    </select>
+  );
+}
+
+export default function Home() {
+  const [answers, setAnswers] = useState<Answer[]>([]);
+  const currentQuestion = questions[answers.length];
+
+  function handleRate(rating: number){
+    setAnswers(answers => answers.concat({
+      ...currentQuestion,
+      valoracion: rating as Answer['valoracion']
+    }))
+  }
+
+  // Mostrar las preguntas y sus valoraciones en caso de haber terminado
+  if (!currentQuestion) {
+    return (
+      <ul>
+        {answers.map((answer) => (
+          <li key={answer.texto}>
+            {answer.texto} - {answer.valoracion}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  return (
+    <main>
       <h1>{currentQuestion.texto}</h1>
-      <select onChange={handleRate}>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-      </select>
+      <Rating 
+        value={1}
+        onChange={handleRate}
+      />
     </main>
-  )
+  );
 }
